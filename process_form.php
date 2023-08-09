b7bf8761f47be24bf26bfd8d9e1cc7857f4701fc
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-08-09 04:08:52 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -2,6 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
 
     $to = $email;
